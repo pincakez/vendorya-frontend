3581e280dba8b0da9ccedd3043732e0e096a72d1
Yakot <pincakez@gmail.com>
2026-06-02 16:59:39 +0300

    config: lock Vite to port 5173 + auto-host for remote tunnel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { VitePWA } from 'vite-plugin-pwa'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
   plugins: [
     vue(),
     VitePWA({
